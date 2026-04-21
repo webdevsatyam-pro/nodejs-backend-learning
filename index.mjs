6369430ebf75e20dac928ea8534d95499d4e8576
webdevsatyam-pro <satyam143142@gmail.com>
2026-04-21 15:07:14 +0530

    dummy signup login added

--- index.mjs
+++ index.mjs
@@ -1,24 +1,46 @@
 import express from 'express'
-import { Prisma } from './prisma/prisma.mjs'
+import { prisma } from './prisma/prisma_client.mjs'
 const app = express()
 const port = 3000
 app.use(express.json)
 
-app.post('/post', async (req, res) => {
-  console.log(res);
-
-  const user = await prisma.user.create(
-    {
-      data:{
-        
-      }
+app.post("/signup", async (req, res) => {
+  console.log(req.body)
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
     }
-  )
-  res.status(200).json({name:'Hello World!'})
+  })
+  res.json(user)
 })
+
+app.post("/login", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email
+    }
+  })
+  if (!user) {
+    res.status(404).json({
+      "error": "user not found"
+    })
+    return
+  }
+  if (user.password !== req.body.password) {
+    res.status(401).json({
+      "error": "password not matched"
+    })
+    return
+  }
+  res.json({ message: "login successful" })
+})
+
 
 app.listen(port, ()=>{
     console.log(`server start from ${port}`)
 })
+
 
 
