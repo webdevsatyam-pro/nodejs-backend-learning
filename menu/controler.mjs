f1b100cd996f3ffd652997540bd003b49b6b62eb
webdevsatyam-pro <satyam143142@gmail.com>
2026-05-11 17:44:43 +0530

    Middleware flow explain

--- menu/controler.mjs
+++ menu/controler.mjs
@@ -2,7 +2,15 @@ import { prisma } from "../prisma/prisma_client.mjs";
 
 const createMenu = async (req, res) => {
   console.log(req.body);
-  const menu = prisma.menu.create(data);
+  const menu = prisma.menu.create({
+    data: {
+      name: req.body.name,
+      description: req.body.description,
+      price_half: req.body.price_half,
+      price_full: req.body.price_full,
+    },
+  });
+  res.json(menu);
 };
 const getMenus = async (req, res) => {
   res.json({ key: "get menus" });
